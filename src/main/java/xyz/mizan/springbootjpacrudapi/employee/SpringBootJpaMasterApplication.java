32bccab7141f7628ff0cf25b330b06ffbd6f05be
kmmizanurrahmanjp <https.mizan@gmail.com>
2018-08-12 20:32:33 +0600

    initial commit

---
+++ src/main/java/xyz/mizan/springbootjpacrudapi/employee/SpringBootJpaMasterApplication.java
@@ -0,0 +1,12 @@
+package xyz.mizan.springbootjpacrudapi.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaMasterApplication.class, args);
+	}
+}
